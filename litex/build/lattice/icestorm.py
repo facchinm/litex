m-labs/migen

build/lattice: Refactor special_overrides as toolchain attributes.

migen/build/lattice/icestorm.py
@@ -9,6 +9,7 @@ from migen.fhdl.structure import _Fragment
 
 from migen.build.generic_platform import *
 from migen.build import tools
+from migen.build.lattice import common
 
 
 def _format_constraint(c):
@@ -123,6 +124,8 @@ class LatticeIceStormToolchain:
         # ice40 does not have a shift register primitive.
         "no_shreg_extract": None
     }
+
+    special_overrides = common.icestorm_special_overrides
 
     def __init__(self):
         self.yosys_opt = "-q"

facchinm/litex

litex/build/lattice/icestorm.py
@@ -9,7 +9,7 @@ from litex.gen.fhdl.structure import _Fragment
 
 from litex.build.generic_platform import *
 from litex.build import tools
-
+from litex.build.lattice import common
 
 def _format_constraint(c):
     pass
@@ -123,6 +123,8 @@ class LatticeIceStormToolchain:
         # ice40 does not have a shift register primitive.
         "no_shreg_extract": None
     }
+
+    special_overrides = common.icestorm_special_overrides
 
     def __init__(self):
         self.yosys_opt = "-q"